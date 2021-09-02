ecf79794fd11164a8b78a8f805fecb28878e971e
Vera <versuchi@yandex.ru>
2021-09-02 10:20:17 +0300

    first components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { SimpleText } from "./components/SimpleText";
 
 function App() {
+  const x = { isDeveloper: true };
+  const skills = [
+    { id: 1, title: "Html" },
+    { id: 2, title: "css" },
+    { id: 3, title: "React" },
+    { id: 4, title: "Vue" },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SimpleText name={"Alex"} age={300} someObj={x} stack={skills} />
     </div>
   );
 }
